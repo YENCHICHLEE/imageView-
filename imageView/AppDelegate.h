6f357f56478debf7a40277f6e12daf1ff6032d95
YENCHICHLEE <k7860710970@hotmail.com>
2015-10-28 16:44:02 +0800

    Initial Commit

---
+++ imageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  imageView
+//
+//  Created by JAY on 2015/10/28.
+//  Copyright (c) 2015年 Huijun.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
